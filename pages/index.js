558a2bec6346fae3c90c08e07f56f0e9c4a18e28
github kt3658 <kt1117tk5@gmail.com>
2022-10-15 11:34:05 +0900

    完了

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Main() {
       <>
   <Head>
     <meta charset="UTF-8"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta content="webポートフォリオサイトです。" name="description"/>
     <title>Ken | Web Portfolio</title>
